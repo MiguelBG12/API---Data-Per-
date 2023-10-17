cca67cc16ad1da98b62663be856c7804a2ede935
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-10-17 13:05:26 -0500

    code app.js

--- api/app.js
+++ api/app.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const db = require('./dbconfig'); // Importa la configuración de la base de datos
-
-const app = express();
-
-// Ruta de ejemplo para obtener datos desde la base de datos
-app.get('/api/ejemplo', async (req, res) => {
-  try {
-    const [rows, fields] = await db.query('SELECT * FROM tabla_ejemplo');
-    res.json(rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al consultar la base de datos' });
-  }
+const mysql = require('db/mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',  // Cambia el host si la base de datos está en otro servidor
+    user: 'root',
+    password: 'root',
+    database: 'database_pruebas',
+});
+// Establece la conexión a la base de datos
+connection.connect((err) => {
+    if (err) {
+        console.error('Error de conexión a la base de datos:', err);
+    } else {
+        console.log('Conexión exitosa a la base de datos');
+    }
 });
 
-const puerto = 3000;
-app.listen(puerto, () => {
-  console.log(`API escuchando en el puerto ${puerto}`);
+// Consulta SQL para recuperar datos
+
+const query = "SELECT Departamento, SUM(TRANSACCIONES) FROM Transacciones WHERE Departamento = 'LIMA' GROUP BY Departamento;";
+connection.query(query, (err, results) => {
+    if (err) {
+        console.error('Error en la consulta:', err);
+    } else {
+        console.log('Resultados de la consulta:', results);
+        // Procesa los resultados o realiza otras operaciones
+    }
 });
+
